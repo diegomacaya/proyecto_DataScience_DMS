5dae182690bcf62db1f4b22323218406672f3a14
Diego Macaya <diego@macaya.cl>
2024-11-12 17:44:14 +0000

    Creada la base de datos

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,10 @@ import pandas as pd
 # load the .env file variables
 load_dotenv()
 
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///Proyecto_Data_Science_VTIindex.db')
+
 
 def db_connect():
     import os
